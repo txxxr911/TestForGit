fabe46572f3e23d261475d1b54d5630e53d80b97
Txxxr <victorucheba@gmail.com>
2022-12-09 12:42:19 +0300

    Initial Commit

---
+++ TestForGit/TestForGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestForGitApp.swift
+//  TestForGit
+//
+//  Created by Victor on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestForGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
